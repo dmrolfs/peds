2f474a032104f1a5afc1d1b76d49519dac33de02
Damon Rolfs <drolfs@gmail.com>
2013-10-01 14:16:35 -0700

    wip

---
+++ commons/src/main/scala/peds/commons/package.scala
@@ -0,0 +1,13 @@
+package peds
+
+import scala.util.{Try, Success, Failure}
+
+
+package object commons {
+  def flatten[T]( xs: Seq[Try[T]] ): Try[Seq[T]] = {
+    val ( ss: Seq[Success[T]]@unchecked, fs: Seq[Failure[T]]@unchecked ) = xs.partition(_.isSuccess)
+
+    if ( fs.isEmpty ) Success( ss map { _.get } )
+    else Failure[Seq[T]]( fs( 0 ).exception ) // Only keep the first failure
+  }
+}
--- commons/src/main/scala/peds/commons/partial/package.scala
+++ commons/src/main/scala/peds/commons/partial/package.scala
@@ -3,6 +3,11 @@ package peds.commons
 import scala.annotation.implicitNotFound
 
 
+/**
+ * The partial package is useful in filtering RESTful interfaces in order to focus a response on only the requried data.
+ * This has several benefits to the operation of APIs. It reduces the bandwidth required to support an API and thereby 
+ * reduces reduces the operational cost for the service. For large scale services this can be significant.
+ */
 package object partial extends XmlReducable with JsonReducable {
   implicit val xmlTransformable = XmlReducable.XmlTransformable
   implicit val jsonTransformable = JsonReducable.JsonTransformable
