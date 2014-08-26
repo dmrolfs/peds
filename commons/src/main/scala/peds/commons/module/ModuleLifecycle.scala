d4ec765e2c1f36af7a695bdafb1f80811ecbc913
Damon Rolfs <damon.rolfs@rightside.co>
2014-08-26 07:41:05 -0700

    removed defaults

--- commons/src/main/scala/peds/commons/module/ModuleLifecycle.scala
+++ commons/src/main/scala/peds/commons/module/ModuleLifecycle.scala
@@ -2,6 +2,6 @@ package peds.commons.module
 
 
 trait ModuleLifecycle {
-  def start( moduleContext: Map[Symbol, Any] = Map() ): Unit = { }
-  def stop( moduleContext: Map[Symbol, Any] = Map() ): Unit = { }
+  def start( moduleContext: Map[Symbol, Any] ): Unit = { }
+  def stop( moduleContext: Map[Symbol, Any] ): Unit = { }
 }
