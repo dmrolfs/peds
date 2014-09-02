f02c021ae7f3e6a75900449d2b7a5267db84d40f
Damon Rolfs <damon.rolfs@rightside.co>
2014-09-01 22:14:25 -0700

    updated ActorStack based on the approach to build the partialfn via an around()

--- akka/src/main/scala/peds/akka/ActorStack.scala
+++ akka/src/main/scala/peds/akka/ActorStack.scala
@@ -1,14 +1,11 @@
 package peds.akka
 
 import akka.actor.Actor
-import akka.persistence.PersistentActor
 
 
 trait ActorStack extends Actor {
-  def wrappedReceive: Receive
-
-  override def receive: Receive = {
-    case c if wrappedReceive.isDefinedAt( c ) => wrappedReceive( c )
-    case c => unhandled( c )
+  def around( r: Receive ): Receive = {
+    case m if r.isDefinedAt( m ) => r( m )
+    case m => unhandled( m )
   }
 }
--- akka/src/main/scala/peds/akka/persistence/PersistentActorStack.scala
+++ akka/src/main/scala/peds/akka/persistence/PersistentActorStack.scala
@@ -1,21 +1,25 @@
-package peds.akka.persistence
+// package peds.akka.persistence
 
-import akka.actor.Actor.Receive
-import akka.persistence.PersistentActor
-import peds.akka.ActorStack
+// import akka.actor.ActorLogging
+// import akka.actor.Actor.Receive
+// import akka.persistence.PersistentActor
 
 
-trait PersistentActorStack extends PersistentActor with ActorStack {
-  def wrappedReceiveCommand: Receive
-  def wrappedReceiveRecover: Receive
+// trait PersistentActorStack extends PersistentActor { outer: ActorLogging =>
+//   def aroundReceiveCommand( r: Receive ): Receive = {
+//     case c if r.isDefinedAt( c ) => {
+// log error s"PersistentActorStack.receiveCommand WRAPPED_RECIEVED IS DEFN for: ${c}"
+//       r( c )
+//     }
+    
+//     case c => {
+// log error s"PersistentActorStack.receiveCommand WRAPPED_RECIEVED IS NOT DEFN for: ${c}"
+//       unhandled( c )
+//     }
+//   }
 
-  override def receiveCommand: Receive = {
-    case c if wrappedReceiveCommand.isDefinedAt( c ) => wrappedReceiveCommand( c )
-    case c => unhandled( c )
-  }
-
-  override def receiveRecover: Receive = {
-    case e if wrappedReceiveRecover.isDefinedAt( e ) => wrappedReceiveRecover( e )
-    case e => unhandled( e )
-  }
-}
+//   def aroundReceiveRecover( r: Receive ): Receive = {
+//     case e if r.isDefinedAt( e ) => r( e )
+//     case e => unhandled( e )
+//   }
+// }
