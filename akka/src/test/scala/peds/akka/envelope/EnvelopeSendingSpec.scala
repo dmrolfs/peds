50aad7cff9172d35a3400cddefd78bbf1262dba6
Damon Rolfs <drolfs@gmail.com>
2014-10-10 21:32:39 -0700

    added sendForward test

--- akka/src/test/scala/peds/akka/envelope/EnvelopeSendingSpec.scala
+++ akka/src/test/scala/peds/akka/envelope/EnvelopeSendingSpec.scala
@@ -20,7 +20,10 @@ class TestActor( target: ActorRef ) extends EnvelopingActor with ActorLogging {
   override def receive: Receive = bare orElse around( wrapped )
 
   def bare: Receive = { case Envelope( "INITIAL", h ) => target ! Envelope( "REPLY", h ) }
-  def wrapped: Receive = { case "ACTOR" => target send "REPLY" }
+  def wrapped: Receive = { 
+    case "ACTOR" => target send "REPLY" 
+    case "FORWARD" => target sendForward "REPLY"
+  }
 
   override def unhandled( message: Any ): Unit = target ! TestActor.Unhandled( message )
 }
@@ -50,9 +53,10 @@ with ImplicitSender
   import peds.commons.util._
 
   test( "EnvelopeSending should wrap a message with an envelope with unknown from non-actor" ) {
-    source send "INITIAL"
+    source.send( "INITIAL" )( probe.ref )
     target.expectMsgPF( max = d, hint = "initial send" ) {
       case Envelope( "REPLY", h ) => {
+        assert( target.sender() == source )
         assert( h.fromComponentType == ComponentType.unknown )
         assert( h.fromComponentPath == ComponentPath.unknown )
         assert( h.toComponentPath == ComponentPath( source.path ) )
@@ -66,9 +70,27 @@ with ImplicitSender
   }
 
   test( "EnvelopeSending should wrap a message with an envelope with source from actor" ) {
-    source send "ACTOR"
+    source.send( "ACTOR" )( probe.ref )
     target.expectMsgPF( max = d, hint = "actor send" ) {
       case Envelope( "REPLY", h ) => {
+        assert( target.sender() == source )
+        assert( h.fromComponentType == ComponentType( classOf[TestActor].safeSimpleName ) )
+        assert( h.fromComponentPath == ComponentPath( source.path ) )
+        assert( h.toComponentPath == ComponentPath( target.ref.path ) )
+        assert( h.messageType == MessageType( classOf[String].safeSimpleName ) )
+        assert( h.workId != WorkId.unknown )
+        assert( h.messageNumber == MessageNumber( 1 ) )
+        assert( h.version == EnvelopeVersion( 1 ) )
+        assert( h.properties == Map.empty )
+      }
+    }
+  }
+
+  test( "EnvelopeSending should wrap a forwarded message with an envelope with source from actor" ) {
+    source.send( "FORWARD" )( probe.ref )
+    target.expectMsgPF( max = d, hint = "actor send" ) {
+      case Envelope( "REPLY", h ) => {
+        assert( target.sender() == probe.ref )
         assert( h.fromComponentType == ComponentType( classOf[TestActor].safeSimpleName ) )
         assert( h.fromComponentPath == ComponentPath( source.path ) )
         assert( h.toComponentPath == ComponentPath( target.ref.path ) )
