60b983254390af20a4b2a36a9a90a19bc6ad97b8
Damon Rolfs <drolfs@gmail.com>
2014-11-04 21:30:07 -0800

    fixed incomplete refactor of local to stream publish strategy

--- akka/src/main/scala/peds/akka/publish/package.scala
+++ akka/src/main/scala/peds/akka/publish/package.scala
@@ -13,15 +13,33 @@ package object publish extends LazyLogging {
 
   private[this] val trace = Trace( "peds.akka.publish", logger )
 
+  /**
+   * Publisher is a chained operation that supports publishing via multiple links. If a publishing link returns Left(event), the
+   * next publishing link will be processed; otherwise if Right(event) is returned then publishing will cease.
+   */
   type Publisher = Chain.Link[Envelope, Unit]
 
 
+  /**
+   * EventPublisher specifies 
+   */
   trait EventPublisher extends ActorStack with ActorLogging {
-    def publish: Publisher = local
+    def publish: Publisher = stream
   }
 
 
-  def local( implicit context: ActorContext ): Publisher = ( event: Envelope ) => trace.block( s"publish.local($event)" ) {
+  /**
+   * Publish event to actor's sender
+   */
+  def sender( implicit context: ActorContext ): Publisher = ( event: Envelope ) => trace.block( s"publish.sender($event)" ) {
+    context.sender() ! event
+    Left( event )
+  }
+
+  /**
+   * Publish event to ActorSystem's eventStream
+   */
+  def stream( implicit context: ActorContext ): Publisher = ( event: Envelope ) => trace.block( s"publish.stream($event)" ) {
     val target = context.system.eventStream
     event.cast[target.Event] foreach { e =>
       logger info s"local stream publishing event:${e} on target:${target}"
@@ -31,5 +49,8 @@ package object publish extends LazyLogging {
     Left( event )
   }
 
+  /**
+   * Throw away published event; i.e., to /dev/null
+   */
   val silent: Publisher = ( event: Envelope ) => trace.block( s"publish.silent($event)" ) { Left( event ) }
 }
