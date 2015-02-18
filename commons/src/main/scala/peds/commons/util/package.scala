d179d0252cb8f4fb8cd5fe83005e76aad2dfc719
Damon Rolfs <drolfs@gmail.com>
2015-02-17 17:10:08 -0800

    added future and option monad transformation operations

--- commons/src/main/scala/peds/commons/util/package.scala
+++ commons/src/main/scala/peds/commons/util/package.scala
@@ -1,7 +1,19 @@
 package peds.commons
 
+import scala.concurrent.{ ExecutionContext, Future }
+
 
 package object util {
+
+  /**
+   * Transforms a function into one that accepts Future arguments and outputs Future results. Used in function composition that pertains
+   * to concurrent operations.
+   * TODO: MOVE INTO PEDS.
+   */
+  def futureT[A, B]( f: A => B )( implicit ec: ExecutionContext ): Future[A] => Future[B] = ( a: Future[A] ) => a map { f }
+
+  def optionT[A, B]( f: A => B ): Option[A] => Option[B] = ( a: Option[A] ) => a map { f }
+
   implicit class SimpleClassNameExposure( val underlying: Class[_] ) extends AnyVal {
     def safeSimpleName: String = {
       underlying.getName.split( '.' ).last.split( '$' ).last
