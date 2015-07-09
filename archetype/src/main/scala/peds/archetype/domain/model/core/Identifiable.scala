895b8da1eaea8f6ac68c61f7b0e5b28a98f2d468
Damon Rolfs <drolfs@gmail.com>
2015-07-09 15:15:09 -0700

    split TID to companion

--- archetype/src/main/scala/peds/archetype/domain/model/core/Identifiable.scala
+++ archetype/src/main/scala/peds/archetype/domain/model/core/Identifiable.scala
@@ -10,11 +10,21 @@ trait Identifiable {
   type ID
   def idClass: Class[_]
 
-  type TID = TaggedID[ID]
-  def idTag: Symbol
-  implicit def tag( id: ID ): TID = TaggedID( idTag, id )
+  type TID = Identifiable.TID[ID]
+  // def idTag: Symbol
+  // implicit def tag( id: ID ): TID = TaggedID( idTag, id )
 
   def id: TID
   // def idLens: Lens[this.type, ID]
   // def idLens: Lens[That, ID]
+}
+
+object Identifiable {
+  type TID[I] = TaggedID[I]
+}
+
+trait IdentifiableCompanion[I <: Identifiable] {
+  def nextId: Identifiable.TID[I#ID]
+  def idTag: Symbol
+  implicit def tag( id: I#ID ): Identifiable.TID[I#ID] = TaggedID( idTag, id )
 }
