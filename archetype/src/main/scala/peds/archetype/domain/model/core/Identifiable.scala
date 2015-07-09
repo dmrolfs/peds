3d1a63809fd2ec49d1cc7f3f142846a8c77c41df
Damon Rolfs <drolfs@gmail.com>
2015-07-09 07:17:19 -0700

    updated to use tagged id

--- archetype/src/main/scala/peds/archetype/domain/model/core/Identifiable.scala
+++ archetype/src/main/scala/peds/archetype/domain/model/core/Identifiable.scala
@@ -1,6 +1,7 @@
 package peds.archetype.domain.model.core
 
 // import shapeless.Lens
+import peds.commons.identifier._
 
 
 trait Identifiable {
@@ -9,7 +10,11 @@ trait Identifiable {
   type ID
   def idClass: Class[_]
 
-  def id: ID
+  type TID = TaggedID[ID]
+  def idTag: Symbol
+  implicit def tag( id: ID ): TID = TaggedID( idTag, id )
+
+  def id: TID
   // def idLens: Lens[this.type, ID]
   // def idLens: Lens[That, ID]
 }
