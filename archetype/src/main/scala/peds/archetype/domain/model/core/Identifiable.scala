49ef7d8c8133883158df31f8a0859ca2a19f0119
Damon Rolfs <drolfs@gmail.com>
2015-09-13 20:45:34 -0700

    removed That, entity expected properties and added EntityCompanion

--- archetype/src/main/scala/peds/archetype/domain/model/core/Identifiable.scala
+++ archetype/src/main/scala/peds/archetype/domain/model/core/Identifiable.scala
@@ -5,18 +5,12 @@ import peds.commons.identifier._
 
 
 trait Identifiable {
-  type That <: Identifiable
-
   type ID
   def idClass: Class[_]
 
   type TID = Identifiable.TID[ID]
-  // def idTag: Symbol
-  // implicit def tag( id: ID ): TID = TaggedID( idTag, id )
 
   def id: TID
-  // def idLens: Lens[this.type, ID]
-  // def idLens: Lens[That, ID]
 }
 
 object Identifiable {
