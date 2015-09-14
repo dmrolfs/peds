5d0b485d1f860de4be3ac876994f3d0c9761454c
Damon Rolfs <drolfs@gmail.com>
2015-09-13 21:23:31 -0700

    removed isActive

--- archetype/src/main/scala/peds/archetype/domain/model/core/Entity.scala
+++ archetype/src/main/scala/peds/archetype/domain/model/core/Entity.scala
@@ -8,7 +8,6 @@ import peds.commons.util._
 trait Entity extends Identifiable {
   def name: String
   def slug: String
-  def isActive: Boolean
 
   override def toString: String = s"${getClass.safeSimpleName}(${name})"
 }
@@ -17,5 +16,4 @@ trait Entity extends Identifiable {
 trait EntityCompanion[E <: Entity] extends IdentifiableCompanion[E] {
   def nameLens: Lens[E, String]
   def slugLens: Lens[E, String]
-  def isActiveLens: Lens[E, Boolean]
 }
