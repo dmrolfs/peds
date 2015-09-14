75fa95bacf66f6df5b051b53666662d24f2a061c
Damon Rolfs <drolfs@gmail.com>
2015-09-14 11:35:41 -0700

    add get convenience operation

--- commons/src/main/scala/peds/commons/identifier/TaggedID.scala
+++ commons/src/main/scala/peds/commons/identifier/TaggedID.scala
@@ -2,6 +2,7 @@ package peds.commons.identifier
 
 //dmr: covariant?
 case class TaggedID[T]( tag: Symbol, id: T ) {
+  def get: T = id
   override lazy val toString: String = s"${tag.name}_${id}"
 }
 
