a4d556e6b8859bee7038a20fa02396e85185d4bd
Damon Rolfs <drolfs@gmail.com>
2016-04-12 10:25:15 -0700

    added recovery to identity matrix (reducing calculation to euclidean) in the event covariance matrix check fails

--- commons/src/main/scala/peds/commons/math/MahalanobisDistance.scala
+++ commons/src/main/scala/peds/commons/math/MahalanobisDistance.scala
@@ -17,7 +17,7 @@ trait MahalanobisDistance extends DistanceMeasure {
   lazy val inverseCovariance: RealMatrix = {
     val solver = new LUDecomposition( covariance ).getSolver
     if ( solver.isNonSingular ) solver.getInverse
-    else new LUDecomposition( MatrixUtils createRealIdentityMatrix dimension ).getSolver.getInverse
+    else new LUDecomposition( MahalanobisDistance.identityMatrix( dimension ) ).getSolver.getInverse
   }
 
   override def compute( centroid: Array[Double], point: Array[Double] ): Double = {
@@ -37,7 +37,11 @@ object MahalanobisDistance {
   }
 
   def fromPoints( group: RealMatrix ): Valid[MahalanobisDistance] = {
-    checkCovariance( group ) map { cov => SimpleMahalanobisDistance( dimension = group.getColumnDimension, covariance = cov ) }
+    checkCovariance( group )
+    .disjunction
+    .recover { case exs => identityMatrix( group.getColumnDimension ) }
+    .map { cov => SimpleMahalanobisDistance( dimension = group.getColumnDimension, covariance = cov ) }
+    .validation
   }
 
   def checkCovarianceMatrixDimension( square: RealMatrix ): Valid[RealMatrix] = {
@@ -55,6 +59,9 @@ object MahalanobisDistance {
   def checkCovariance( group: RealMatrix ): Valid[RealMatrix] = {
     Validation.fromTryCatchNonFatal{ new Covariance( group ).getCovarianceMatrix }.toValidationNel
   }
+
+  def identityMatrix( dimension: Int ): RealMatrix = MatrixUtils createRealIdentityMatrix dimension
+
 
   final case class SimpleMahalanobisDistance private[math](
     override val dimension: Int,
