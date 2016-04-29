bc422cd83cd02788b1bb301a3cc9833bb353d619
Damon Rolfs <drolfs@gmail.com>
2016-04-29 16:14:43 -0700

    change back to max in flight request strategy. watermark approach was too free

--- akka/src/main/scala/peds/akka/stream/ProcessorAdapter.scala
+++ akka/src/main/scala/peds/akka/stream/ProcessorAdapter.scala
@@ -8,8 +8,10 @@ import akka.event.LoggingReceive
 import akka.stream.actor._
 import akka.stream.scaladsl._
 import akka.stream.{FlowShape, Graph, Materializer}
+import com.codahale.metrics.{Metric, MetricFilter}
 import nl.grons.metrics.scala.{Meter, MetricName}
 import peds.akka.metrics.InstrumentedActor
+import peds.commons.collection.BloomFilter
 
 
 /**
@@ -76,15 +78,16 @@ object ProcessorAdapter {
       }
 
       val processorPublisher = b.add( Source fromPublisher[O] publisher )
-      val egress = b.add( Flow[O] map { identity } )
+//      val egress = b.add( Flow[O] map { identity } )
 
-      processorPublisher ~> egress
+//      processorPublisher ~> egress
 
       val adapter = b.add(
         Sink.actorSubscriber[I]( adapterPropsFromPublisher( publisherRef ) ).named( s"${label.name}-processor-ingress")
       )
 
-      FlowShape( adapter.in, egress.out )
+//      FlowShape( adapter.in, egress.out )
+      FlowShape( adapter.in, processorPublisher.out )
     }
   }
 
@@ -133,21 +136,61 @@ object ProcessorAdapter {
 class ProcessorAdapter extends ActorSubscriber with InstrumentedActor with ActorLogging {
   outer: ProcessorAdapter.TopologyProvider =>
 
+  override def preStart(): Unit = initializeMetrics()
+
   override lazy val metricBaseName: MetricName = MetricName( classOf[ProcessorAdapter] )
   val submissionMeter: Meter = metrics meter "submission"
+  val publishMeter: Meter = metrics meter "publish"
 
-  override protected def requestStrategy: RequestStrategy = {
-    new WatermarkRequestStrategy( highWatermark = (Runtime.getRuntime.availableProcessors() * outer.maxInFlightCpuFactor).toInt )
+  def initializeMetrics(): Unit = {
+    stripLingeringMetrics()
+    metrics.gauge( "outstanding" ) { outstanding }
   }
 
-  override def receive: Receive = LoggingReceive { around( withSubscriber(outer.destinationPublisher) ) }
+  def stripLingeringMetrics(): Unit = {
+    metrics.registry.removeMatching(
+      new MetricFilter {
+        override def matches( name: String, metric: Metric ): Boolean = {
+          name.contains( classOf[ProcessorAdapter].getName ) && name.contains( "outstanding" )
+        }
+      }
+    )
+  }
+
+
+  var outstanding: Int = 0
+
+//  val seenMax: Int = 1000000
+//  var seen: BloomFilter[Any] = BloomFilter[Any]( maxFalsePosProbability = 0.01, seenMax )
+  def reportDuplicates( toBePublished: Iterable[Any] ): Unit = {
+//    toBePublished foreach { tbp =>
+//      if ( seen.size == seenMax ) log.warning( "[ProcessAdapter] SEEN filled" )
+//      if ( seen.size < seenMax ) {
+//        if ( seen has_? tbp ) log.warning( "[ProcessorAdapter] Possible duplicate: [{}]", tbp)
+//        else seen += tbp
+//      }
+//    }
+  }
+
+  override protected def requestStrategy: RequestStrategy = {
+    new MaxInFlightRequestStrategy( max = outer.maxInFlight ) {
+      override def inFlightInternally: Int = outstanding
+    }
+  }
+
+
+  override def receive: Receive = LoggingReceive {
+    around( withSubscriber(outer.destinationPublisher) orElse publish(outer.destinationPublisher) )
+  }
 
   def withSubscriber( subscriber: ActorRef ): Receive = {
     case next @ ActorSubscriberMessage.OnNext( message ) if outer.workerFor.isDefinedAt( message ) => {
+      reportDuplicates( Seq(message) )
       submissionMeter.mark()
+      outstanding += 1
       val worker = outer workerFor message
       context watch worker
-      worker.tell( message, subscriber )
+      worker ! message
     }
 
     case ActorSubscriberMessage.OnComplete => outer.destinationPublisher ! ActorSubscriberMessage.OnComplete
@@ -155,9 +198,18 @@ class ProcessorAdapter extends ActorSubscriber with InstrumentedActor with Actor
     case onError: ActorSubscriberMessage.OnError => outer.destinationPublisher ! onError
 
     case Terminated( deadWorker ) => {
+
       log.error( "Flow Processor notified of worker death: [{}]", deadWorker )
       //todo is this response appropriate for spotlight and generally?
 //      outer.destinationPublisher ! ActorSubscriberMessage.OnError( ProcessorAdapter.DeadWorkerError(deadWorker) )
+    }
+  }
+
+  def publish( subscriber: ActorRef ): Receive = {
+    case message => {
+      publishMeter.mark()
+      outstanding -= 1
+      subscriber ! message
     }
   }
 }
