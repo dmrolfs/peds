238aa71de0fccce9258e82b66e431d7b8a8cc960
Damon Rolfs <drolfs@gmail.com>
2016-05-17 20:45:40 -0700

    set classname as default metric base name

--- akka/src/main/scala/peds/akka/metrics/Instrumented.scala
+++ akka/src/main/scala/peds/akka/metrics/Instrumented.scala
@@ -1,14 +1,16 @@
 package peds.akka.metrics
 
 import com.codahale.metrics.MetricRegistry
-import nl.grons.metrics.scala.InstrumentedBuilder
+import nl.grons.metrics.scala.{HdrMetricBuilder, InstrumentedBuilder, MetricName}
 
 
 /**
   * Created by rolfsd on 12/15/15.
   */
 trait Instrumented extends InstrumentedBuilder {
+  override lazy val metricBaseName: MetricName = MetricName( this.getClass )
   override implicit val metricRegistry: MetricRegistry = Instrumented.metricRegistry
+  override lazy protected val metricBuilder = new HdrMetricBuilder( metricBaseName, metricRegistry, resetAtSnapshot = false )
 }
 
 object Instrumented {
