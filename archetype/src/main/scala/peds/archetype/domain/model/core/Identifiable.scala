42910380ae1d6844369d993a4ddd782463ea5d99
Damon Rolfs <drolfs@gmail.com>
2016-06-08 09:27:26 -0700

    switch from idClass: Class[_] to evId: ClassTag[ID]

--- archetype/src/main/scala/peds/archetype/domain/model/core/Identifiable.scala
+++ archetype/src/main/scala/peds/archetype/domain/model/core/Identifiable.scala
@@ -3,23 +3,20 @@ package peds.archetype.domain.model.core
 import shapeless.Lens
 import peds.commons.identifier._
 
+import scala.reflect.ClassTag
+
 
 trait Identifiable {
   type ID
-  def idClass: Class[_]
+  def evId: ClassTag[ID]
 
   type TID = TaggedID[ID]
 
   def id: TID
 }
 
-// object Identifiable {
-//   type TID[I] = TaggedID[I]
-// }
 
 trait IdentifiableCompanion[I <: Identifiable] {
-  // type ID = I#ID
-  // type TID = I#TID
   def nextId: I#TID
   def idTag: Symbol
   implicit def tag( id: I#ID ): I#TID
