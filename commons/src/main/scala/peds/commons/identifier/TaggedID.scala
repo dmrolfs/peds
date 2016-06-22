721a70438d2e6169bbc43476ecd6b7028601356f
Damon Rolfs <drolfs@gmail.com>
2016-06-22 15:11:00 -0700

    added extractor for tagged id. need to test and uncertain will be used :-)

--- commons/src/main/scala/peds/commons/identifier/TaggedID.scala
+++ commons/src/main/scala/peds/commons/identifier/TaggedID.scala
@@ -1,11 +1,15 @@
 package peds.commons.identifier
 
-//dmr: covariant?
-case class TaggedID[T]( tag: Symbol, id: T ) {
+
+case class TaggedID[+T]( tag: Symbol, id: T ) {
   def get: T = id
-  override lazy val toString: String = s"${tag.name}_${id}"
+  override lazy val toString: String = tag.name + TaggedID.Delimiter + id.toString
 }
 
 object TaggedID {
+  def unapply( arg: TaggedID[_] ): Option[(Symbol, Any)] = Some( (arg.tag, arg.id) )
+
+  val Delimiter: String = "-"
+  val Regex = s"""((.+)${Delimiter})?(.+)""".r  // this breaks down if Delimiter needs to be escaped
   implicit def taggedId2Id[T]( tid: TaggedID[T] ): T = tid.id
 }
