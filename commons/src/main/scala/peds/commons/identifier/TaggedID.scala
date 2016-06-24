d41f0881e56affb6b02d573b8341e3d55a67fa9c
Damon Rolfs <drolfs@gmail.com>
2016-06-24 08:28:09 -0700

    remove extractor since it conflicted with one provided with case class

--- commons/src/main/scala/peds/commons/identifier/TaggedID.scala
+++ commons/src/main/scala/peds/commons/identifier/TaggedID.scala
@@ -7,8 +7,6 @@ case class TaggedID[+T]( tag: Symbol, id: T ) {
 }
 
 object TaggedID {
-  def unapply( arg: TaggedID[_] ): Option[(Symbol, Any)] = Some( (arg.tag, arg.id) )
-
   val Delimiter: String = "-"
   val Regex = s"""((.+)${Delimiter})?(.+)""".r  // this breaks down if Delimiter needs to be escaped
   implicit def taggedId2Id[T]( tid: TaggedID[T] ): T = tid.id
