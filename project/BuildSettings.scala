b3a60ba87a05496af3427d1e633c6c3c9e998f7a
Damon Rolfs <drolfs@gmail.com>
2016-07-01 15:48:25 -0700

    bump version in progress and assign third point for hot fixes

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -5,7 +5,7 @@ import org.scoverage.coveralls.Imports.CoverallsKeys._
 
 object BuildSettings {
 
-  val VERSION = "0.2.7"
+  val VERSION = "0.3.0-SNAPSHOT"
 
   lazy val noPublishing = Seq(
     publish := (),
