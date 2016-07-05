d39ca37012f4e7a3bb31e012a7b32c784b51a672
Damon Rolfs <drolfs@gmail.com>
2016-07-05 16:11:23 -0700

    bump version

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -5,7 +5,7 @@ import org.scoverage.coveralls.Imports.CoverallsKeys._
 
 object BuildSettings {
 
-  val VERSION = "0.3.0"
+  val VERSION = "0.3.1-SNAPSHOT"
 
   lazy val noPublishing = Seq(
     publish := (),
