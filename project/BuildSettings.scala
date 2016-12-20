6b056587bd21e7b59b964e369213f15ffcbb4e21
Damon Rolfs <drolfs@gmail.com>
2016-12-19 23:46:07 -0800

    snapshot version

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -5,7 +5,7 @@ import org.scoverage.coveralls.Imports.CoverallsKeys._
 
 object BuildSettings {
 
-  val VERSION = "0.4.3"
+  val VERSION = "0.4.4-SNAPSHOT"
 
   lazy val noPublishing = Seq(
     publish := (),
