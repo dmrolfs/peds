800939b45d6b7e8879d563a7e280401cb20c0bfd
Damon Rolfs <drolfs@gmail.com>
2017-01-25 13:47:55 -0800

    release version

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -5,7 +5,7 @@ import org.scoverage.coveralls.Imports.CoverallsKeys._
 
 object BuildSettings {
 
-  val VERSION = "0.4.4"
+  val VERSION = "0.4.5"
 
   lazy val noPublishing = Seq(
     publish := (),
