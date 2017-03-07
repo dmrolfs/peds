61ff1910721b61197ead5c1131388325f0fb2472
Damon Rolfs <drolfs@gmail.com>
2017-03-06 17:36:41 -0800

    turns out hash ids is really bad for shortUUID performance. reverted to original impl

--- commons/src/main/scala/omnibus/commons/identifier/ShortUUID.scala
+++ commons/src/main/scala/omnibus/commons/identifier/ShortUUID.scala
@@ -5,30 +5,31 @@ import scala.reflect._
 import scalaz._, Scalaz._
 import org.apache.commons.codec.binary.Base64
 import com.eaio.uuid.UUID
-import org.hashids._
-import org.hashids.syntax._
+// import org.hashids._
+// import org.hashids.syntax._
 import omnibus.commons.TryV
 
 
 object ShortUUID {
-  implicit val hashids: Hashids = Hashids.reference( "omnibus comprises several items" )
+  // implicit val hashids: Hashids = Hashids.reference( "omnibus comprises several items" )
 
   def apply(): ShortUUID = fromUUID( new UUID )
-  def fromString( rep: String ): ShortUUID = new ShortUUID( rep.unhashid )
+  def fromString( rep: String ): ShortUUID = new ShortUUID( rep )
+  // def fromString( rep: String ): ShortUUID = new ShortUUID( rep.unhashid )
 
   implicit def fromUUID( uuid: UUID ): ShortUUID = {
-    val clock: Long = uuid.getClockSeqAndNode
-    val isClockNegative: Long = if ( clock < 0L ) 1L else 0L
-    val time: Long = uuid.getTime
-    val isTimeNegative: Long = if ( time < 0L ) 1L else 0L
-    new ShortUUID( List(isClockNegative, math.abs(clock), isTimeNegative, math.abs(time)) )
-    // val bb = ByteBuffer allocate 16
-    // bb putLong uuid.getClockSeqAndNode
-    // bb putLong uuid.getTime
-    // bb.flip
-    // val result = ShortUUID( Base64 encodeBase64URLSafeString bb.array )
-    // bb.clear
-    // result
+    // val clock: Long = uuid.getClockSeqAndNode
+    // val isClockNegative: Long = if ( clock < 0L ) 1L else 0L
+    // val time: Long = uuid.getTime
+    // val isTimeNegative: Long = if ( time < 0L ) 1L else 0L
+    // new ShortUUID( List(isClockNegative, math.abs(clock), isTimeNegative, math.abs(time)) )
+    val bb = ByteBuffer allocate 16
+    bb putLong uuid.getClockSeqAndNode
+    bb putLong uuid.getTime
+    bb.flip
+    val result = new ShortUUID( Base64 encodeBase64URLSafeString bb.array )
+    bb.clear
+    result
   }
 
   implicit def toUUID( short: ShortUUID ): UUID = short.toUUID
@@ -43,27 +44,28 @@ object ShortUUID {
   }
 }
 
-class ShortUUID( ids: Seq[Long] ) extends Equals {
-  import ShortUUID.hashids
+class ShortUUID private[identifier]( val repr: String ) extends Equals {
+// class ShortUUID( ids: Seq[Long] ) extends Equals {
+  // import ShortUUID.hashids
 
-  lazy val rep: String = ids.hashid
+  // lazy val rep: String = ids.hashid
 
   def isNil: Boolean = this == ShortUUID.zero
 
   def toUUID: UUID = {
     @inline def actual( magnitude: Long, isNegative: Long ): Long = magnitude * ( if (isNegative == 1L) -1L else 1L )
 
-    val List( isClockNegative, clockMagnitude, isTimeNegative, timeMagnitude ) = ids
-    val clock = actual( clockMagnitude, isClockNegative )
-    val time = actual( timeMagnitude, isTimeNegative )
-    new UUID( time, clock )
-    // val bytes = Base64 decodeBase64 value
-    // val bb = ByteBuffer allocate 16
-    // bb put bytes
-    // bb.flip
-    // val clock = bb.getLong
-    // val time = bb.getLong
+    // val List( isClockNegative, clockMagnitude, isTimeNegative, timeMagnitude ) = ids
+    // val clock = actual( clockMagnitude, isClockNegative )
+    // val time = actual( timeMagnitude, isTimeNegative )
     // new UUID( time, clock )
+    val bytes = Base64 decodeBase64 repr
+    val bb = ByteBuffer allocate 16
+    bb put bytes
+    bb.flip
+    val clock = bb.getLong
+    val time = bb.getLong
+    new UUID( time, clock )
   }
 
   override def canEqual( rhs: Any ): Boolean = rhs.isInstanceOf[ShortUUID]
@@ -72,31 +74,33 @@ class ShortUUID( ids: Seq[Long] ) extends Equals {
     case that: ShortUUID => {
       if ( this eq that ) true
       else {
-        val List( lhsIsClockNegative, lhsClockMagnitude, lhsIsTimeNegative, lhsTimeMagnitude ) = this.ids
-        val List( rhsIsClockNegative, rhsClockMagnitude, rhsIsTimeNegative, rhsTimeMagnitude ) = that.rep.unhashid
+        // val List( lhsIsClockNegative, lhsClockMagnitude, lhsIsTimeNegative, lhsTimeMagnitude ) = this.ids
+        // val List( rhsIsClockNegative, rhsClockMagnitude, rhsIsTimeNegative, rhsTimeMagnitude ) = that.rep.unhashid
 
         ( that.## == this.## ) &&
         ( that canEqual this ) &&
-        ( lhsTimeMagnitude == rhsTimeMagnitude ) &&
-        ( lhsClockMagnitude == rhsClockMagnitude ) &&
-        ( lhsIsClockNegative == rhsIsClockNegative ) &&
-        ( lhsIsTimeNegative == lhsIsTimeNegative )
+        ( that.repr == this.repr )
+        // ( lhsTimeMagnitude == rhsTimeMagnitude ) &&
+        // ( lhsClockMagnitude == rhsClockMagnitude ) &&
+        // ( lhsIsClockNegative == rhsIsClockNegative ) &&
+        // ( lhsIsTimeNegative == lhsIsTimeNegative )
       }
     }
 
     case _ => false
   }
 
-  override val hashCode: Int = {
-    val List( isClockNegative, clockMagnitude, isTimeNegative, timeMagnitude ) = ids
-    41 * ( 
-      41 * (
-        41 * (
-          41 + isClockNegative.##
-        ) + clockMagnitude.##
-      ) + isTimeNegative.##
-    ) + timeMagnitude.##
-  }
+  override val hashCode: Int = 41 * ( 41 + repr.## )
+  // override val hashCode: Int = {
+  //   val List( isClockNegative, clockMagnitude, isTimeNegative, timeMagnitude ) = ids
+  //   41 * (
+  //     41 * (
+  //       41 * (
+  //         41 + isClockNegative.##
+  //       ) + clockMagnitude.##
+  //     ) + isTimeNegative.##
+  //   ) + timeMagnitude.##
+  // }
 
-  override def toString: String = if ( this == ShortUUID.zero ) "<zero-uuid>" else rep
+  override def toString: String = if ( this == ShortUUID.zero ) "<zero-uuid>" else repr
 }
