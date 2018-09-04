3b98b60ec1f3d280afbe8939eacce9f14ac745e8
Damon Rolfs <drolfs@gmail.com>
2018-09-04 15:19:58 -0700

    updated test for identifying

--- identifier/src/test/scala/omnibus/identifier/IdentifyingSpec.scala
+++ identifier/src/test/scala/omnibus/identifier/IdentifyingSpec.scala
@@ -46,15 +46,15 @@ class IdentifyingSpec extends WordSpec with Matchers {
     "identifying should work with optional state entity types" taggedAs WIP in {
       def makeOptionZedId()( implicit i: Identifying[Option[Zed]] ): i.TID = i.next
       def makeOptionZedIdAux()( implicit i: Identifying.Aux[Option[Zed], UUID] ): i.TID = i.next
-      def makeOptionZedIdEntityAux()(
-        implicit i: Identifying.EntityAux[Zed, UUID]
-      ): i.TID = i.next
+//      def makeOptionZedIdEntityAux()(
+//        implicit i: Identifying.Full[Zed, UUID]
+//      ): i.TID = i.next
       def makeOptionZedIdFullAux()(
         implicit i: Identifying.FullAux[Option[Zed], Zed, UUID]
       ): i.TID = i.next
 
-      val o1 = Identifying[Option[Zed]]
-      val oid1 = o1.next
+//      val o1 = Identifying[Option[Zed]]
+//      val oid1 = o1.next
 //      val zid: Id[Zed] = oid1
 //      val zida: Id.Aux[Zed, UUID] = oid1
 
@@ -71,13 +71,13 @@ class IdentifyingSpec extends WordSpec with Matchers {
       "val z: Id.Aux[Zed, UUID] = ozidAux" should compile
       "Zed( id = makeOptionZedIdAux(), score = 3.14 )" should compile
 
-      val foo: Identifying.EntityAux[Zed, UUID] = Zed.identifying
+//      val foo: Identifying.EntityAux[Zed, UUID] = Zed.identifying
 
-      val ozidEntityAux = makeOptionZedIdEntityAux()
-      scribe.info( s"ozidEntityAux = ${ozidEntityAux}" )
-//      val z: Id[Zed] = ozidEntityAux
-      "val z: Id.Aux[Zed, UUID] = ozidEntityAux" should compile
-      "Zed( id = makeOptionZedIdEntityAux(), score = 3.14 )" should compile
+//      val ozidEntityAux = makeOptionZedIdEntityAux()
+//      scribe.info( s"ozidEntityAux = ${ozidEntityAux}" )
+////      val z: Id[Zed] = ozidEntityAux
+//      "val z: Id.Aux[Zed, UUID] = ozidEntityAux" should compile
+//      "Zed( id = makeOptionZedIdEntityAux(), score = 3.14 )" should compile
 
       val ozidFullAux = makeOptionZedIdFullAux()
       scribe.info( s"ozidFullAux = ${ozidFullAux}" )
@@ -146,75 +146,76 @@ class IdentifyingSpec extends WordSpec with Matchers {
       oFooId.getClass shouldBe fooId.getClass
     }
 
-    //    "create Id of varying types" in {
-////      import Foo._
-//
-//      val suid = ShortUUID()
-//      val fid: Id[Foo] = Id of suid
+    "create Id of varying types" in {
+//      import Foo._
+
+      val suid = ShortUUID()
+      val fid: Id[Foo] = Id of suid
 //      fid shouldBe a[Id[Foo]]
-//      fid.toString shouldBe s"FooId(${suid})"
-//      fid.value shouldBe a[ShortUUID]
-//      fid.value shouldBe suid
-//
-//      fid.value shouldBe suid
-//      suid shouldBe fid.value
-//
-//      val bid: Id[Bar] = Id of 13L
+      fid.toString shouldBe s"FooId(${suid})"
+      fid.value shouldBe a[ShortUUID]
+      fid.value shouldBe suid
+
+      fid.value shouldBe suid
+      suid shouldBe fid.value
+
+      val bid: Id[Bar] = Id of 13L
 //      bid shouldBe a[Id[Bar]]
-//      bid.toString shouldBe "BarId(13)"
-//      bid.value.getClass shouldBe classOf[java.lang.Long]
-//      bid.value shouldBe 13L
-//    }
-//
-//    "invalid id type should fail" in {
-//      "val fid: Id[Foo] = Id of 17L" shouldNot compile
-//    }
-//
-//    "create Id from strings" in {
-//      val fid = ShortUUID()
-//      val frep = fid.toString
-//
-//      val f: Id[Foo] = Id fromString frep
+      bid.toString shouldBe "BarId(13)"
+      bid.value.getClass shouldBe classOf[java.lang.Long]
+      bid.value shouldBe 13L
+    }
+
+    "invalid id type should fail" in {
+      "val fid: Id[Foo] = Id of 17L" shouldNot compile
+    }
+
+    "create Id from strings" in {
+      val fid = ShortUUID()
+      val frep = fid.toString
+
+      val f: Id[Foo] = Id fromString frep
 //      f shouldBe a[Id[Foo]]
-//      f.toString shouldBe s"FooId(${fid})"
-//      f.value shouldBe a[ShortUUID]
-//      f.value shouldBe fid
-//
-//      val bid = 17L
-//      val brep = bid.toString
-//      val b: Id[Bar] = Id fromString brep
+      f.toString shouldBe s"FooId(${fid})"
+      f.value shouldBe a[ShortUUID]
+      f.value shouldBe fid
+
+      val bid = 17L
+      val brep = bid.toString
+      val b: Id[Bar] = Id fromString brep
 //      b shouldBe a[Id[Bar]]
-//      b.toString shouldBe s"BarId(${bid})"
-//      b.value.getClass shouldBe classOf[java.lang.Long]
-//      b.value shouldBe bid
-//    }
-//
-//    "invalid id rep type should fail" in {
-//      val fid = 17L
-//      val frep = fid.toString
-//      an[IllegalArgumentException] should be thrownBy Id.fromString[Foo]( frep )
-//    }
-//
-//    "custom labeling can override class label" in {
-//      implicit val fooLabeling = new CustomLabeling[Foo] {
-//        override def label: String = "SPECIAL_FOO"
-//      }
-//
-//      val suid = ShortUUID()
-//      val fid = Id.of[Foo, ShortUUID]( suid )
-//      fid.toString shouldBe s"SPECIAL_FOO(${suid})"
-//
-//      implicit val barLabeling = new EmptyLabel[Bar]
-//      val bid = Id.of[Bar, Long]( 17L )
-//      bid.toString shouldBe "17"
-//    }
-//
-//    "extract id value from Id" in {
-//      val expected = ShortUUID()
-//      val fid: Id[Foo] = Id of expected
-//      val Id( actual ) = fid
-//      actual shouldBe expected
-//      actual shouldBe a[ShortUUID]
-//    }
+      b.toString shouldBe s"BarId(${bid})"
+      b.value.getClass shouldBe classOf[java.lang.Long]
+      b.value shouldBe bid
+    }
+
+    "invalid id rep type should fail" in {
+      val fid = 17L
+      val frep = fid.toString
+
+      an[IllegalArgumentException] should be thrownBy Id.fromString[Foo, ShortUUID]( frep )
+    }
+
+    "custom labeling can override class label" in {
+      implicit val fooLabeling = new CustomLabeling[Foo] {
+        override def label: String = "SPECIAL_FOO"
+      }
+
+      val suid = ShortUUID()
+      val fid = Id.of[Foo, ShortUUID]( suid )
+      fid.toString shouldBe s"SPECIAL_FOO(${suid})"
+
+      implicit val barLabeling = new EmptyLabel[Bar]
+      val bid = Id.of[Bar, Long]( 17L )
+      bid.toString shouldBe "17"
+    }
+
+    "extract id value from Id" in {
+      val expected = ShortUUID()
+      val fid: Id[Foo] = Id of expected
+      val Id( actual ) = fid
+      actual shouldBe expected
+      actual shouldBe a[ShortUUID]
+    }
   }
 }
