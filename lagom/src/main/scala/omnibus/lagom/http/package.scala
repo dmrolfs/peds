0d4c91bddef9b402d5920f83ff5a1b176bd34f18
Damon Rolfs <drolfs@gmail.com>
2018-11-14 12:41:27 -0800

    Add Accepted response value

--- lagom/src/main/scala/omnibus/lagom/http/package.scala
+++ lagom/src/main/scala/omnibus/lagom/http/package.scala
@@ -1,3 +1,14 @@
-package omnibus.lagom package object http {
+package omnibus.lagom
 
+import scala.collection.immutable
+import play.api.http.Status
+import com.lightbend.lagom.scaladsl.api.transport.{MessageProtocol, ResponseHeader}
+
+
+package object http {
+  val Accepted: ResponseHeader = ResponseHeader(
+    status = Status.ACCEPTED,
+    protocol = MessageProtocol.empty,
+    headers = immutable.Seq.empty[( String, String )]
+  )
 }
