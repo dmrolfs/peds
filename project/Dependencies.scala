8e874d3b2c2204af10ba34f7829e10eb6fd25927
Scala Steward <me@scala-steward.org>
2019-07-20 09:48:34 +0200

    Update newtype to 0.4.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -195,7 +195,7 @@ object Dependencies {
   }
 
   val enumeratum = "com.beachape" %% "enumeratum" % "1.5.13"
-  val newtype = "io.estatico" %% "newtype" % "0.4.2"
+  val newtype = "io.estatico" %% "newtype" % "0.4.3"
   val snowflake = "com.softwaremill.common" %% "id-generator" % "1.2.0"
 //    val guava = "com.google.guava" % "guava" % "21.0"
   // val offheap = "sh.den" % "scala-offheap_2.11" % "0.1"
