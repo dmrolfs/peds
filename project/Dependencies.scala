69cbf1edd879b671ec84dcce3aae0647a4c18fe2
Scala Steward <me@scala-steward.org>
2019-08-16 02:13:58 +0200

    Update scala-uuid to 0.3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -204,7 +204,7 @@ object Dependencies {
   val json4sJackson = "org.json4s" %% "json4s-jackson" % "3.6.7"
 //    val uuid = "com.eaio.uuid" % "uuid" % "3.4"
 //    val eaioUuid = "com.github.stephenc.eaio-uuid" % "uuid" % "3.4.2"
-  val scalaUuid = "io.jvm.uuid" %% "scala-uuid" % "0.3.0"
+  val scalaUuid = "io.jvm.uuid" %% "scala-uuid" % "0.3.1"
   val config = "com.typesafe" % "config" % "1.3.4"
   val ficus = "com.iheart" %% "ficus" % "1.4.7"
   val shapeless = "com.chuusai" %% "shapeless" % "2.3.3" withSources() withJavadoc()
