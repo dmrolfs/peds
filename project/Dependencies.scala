31bdd5877f49faac780d541736745b46077c3516
Scala Steward <me@scala-steward.org>
2020-04-26 18:17:38 +0200

    Update nscala-time to 2.24.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -123,7 +123,7 @@ object Dependencies {
   object time {
     val joda = "joda-time" % "joda-time" % "2.10.4"
     val jodaConvert = "org.joda" % "joda-convert" % "2.2.1"
-    val scalaTime = "com.github.nscala-time" %% "nscala-time" % "2.22.0"
+    val scalaTime = "com.github.nscala-time" %% "nscala-time" % "2.24.0"
     def all = Seq( joda, jodaConvert, scalaTime )
   }
 
