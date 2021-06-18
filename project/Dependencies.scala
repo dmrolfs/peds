799bbe1ba707e2c8d904f531866cfae3a4a78560
Scala Steward <me@scala-steward.org>
2021-06-18 08:09:48 +0200

    Update log4j-over-slf4j, slf4j-api to 1.7.31

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -138,8 +138,8 @@ object Dependencies {
     }
 
     val journal = "io.verizon.journal" %% "core" % "3.0.19"
-    val slf4j = "org.slf4j" % "slf4j-api" % "1.7.30" intransitive
-    val log4jOverSlf4j = "org.slf4j" % "log4j-over-slf4j" % "1.7.30"
+    val slf4j = "org.slf4j" % "slf4j-api" % "1.7.31" intransitive
+    val log4jOverSlf4j = "org.slf4j" % "log4j-over-slf4j" % "1.7.31"
 
 //    object scribe extends SimpleModule {
 //      override val groupId: String = "com.outr"
